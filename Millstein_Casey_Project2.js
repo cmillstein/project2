a88eddaa2b445f5b5c05322ed9e7601bb082c9db
cmillstein <cmillstein@fullsail.edu>
2014-04-09 20:02:59 -0400

    Update Millstein_Casey_Project2.js

--- Millstein_Casey_Project2.js
+++ Millstein_Casey_Project2.js
@@ -1,1 +1,65 @@
-alert("I've made a change!");
+// alert("This is my second project!");
+// Casey Millstein
+// SDI 1404
+// Project 2
+
+
+/* 
+Cooking
+Will you be cooking banana bread tonight? (y/n)
+If Y: output yes, return true
+else: output no; return false
+
+confirm: do you know what ingredients you need (ok = yes, cancel = no)
+run booleanFunction()
+
+How many servings do I need
+run numberFunction()
+*/
+
+
+
+
+// My Variables
+var cookingQuestion, ingredientsConfirm;
+
+
+// My Functions
+function procedureFunction(cookingQuestion) {
+	
+}
+
+function booleanFunction(confirm, arg) {
+	var ingredientsNeeded = ["eggs", "milk", "banana", "flour", "cinnamon", "sugar"];
+	var ingredientsHave = ["eggs", "flour", "sugar"];
+	var i,o;
+	for ( i = 0; i<= ingredientsNeed.length; i++) {
+		for( o = 0; o <= ingredientsHave.length; o++) {
+			if (ingredientsHave[o] == ingredientsNeed[i]) {
+				console.log('I have '+ ingredientsHave[o] );
+				break;
+			}
+		}
+		console.log('I need '+ ingredientsNeed[i] ); 
+	}
+	return true;
+}
+
+function numberFunction(servings) {
+	var ingredientsPerServing 
+}
+
+
+// My Outputs
+
+
+
+
+
+
+
+
+
+
+
+// My Conditionals

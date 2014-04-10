7cb4c7c1fb245d1207b8161812516069e750dc58
cmillstein <cmillstein@fullsail.edu>
2014-04-09 20:34:23 -0400

    Update Millstein_Casey_Project2.js

--- Millstein_Casey_Project2.js
+++ Millstein_Casey_Project2.js
@@ -26,33 +26,88 @@ var cookingQuestion, ingredientsConfirm;
 
 // My Functions
 function procedureFunction(cookingQuestion) {
-	
+	if (cookingQuestion == 'yes') {
+		console.log('Yes, time to do it!');
+		return true;
+	} else {
+		console.log('Oh no!');
+		return false;
+	}
 }
 
-function booleanFunction(confirm, arg) {
+function booleanFunction(confirm, arg = "1") {
 	var ingredientsNeeded = ["eggs", "milk", "banana", "flour", "cinnamon", "sugar"];
 	var ingredientsHave = ["eggs", "flour", "sugar"];
-	var i,o;
-	for ( i = 0; i<= ingredientsNeed.length; i++) {
-		for( o = 0; o <= ingredientsHave.length; o++) {
+	var i = 0,o = 0, needSomething = false;
+	
+	while (i <= ingredientsNeeded) {
+		while (o <= ingredientsHave) {
 			if (ingredientsHave[o] == ingredientsNeed[i]) {
 				console.log('I have '+ ingredientsHave[o] );
 				break;
 			}
+			o++;
 		}
-		console.log('I need '+ ingredientsNeed[i] ); 
+		console.log('I need '+ ingredientsNeed[i] );
+		needSomething = true;
+		i++;
 	}
-	return true;
+	
+	if (needSomething)
+		return false;
+	else
+		return true;
 }
 
 function numberFunction(servings) {
-	var ingredientsPerServing 
+	// Per serving ingredients
+	var eggsNeeded = 2, milkNeeded = 1, bananaNeeded = 1, flourNeeded = 0.5, cinnamonNeeded = 0.25, sugarNeeded = 0.5;
+	// total ingredients
+	var eggs, milk, banana, flour, cinnamon, sugar;
+	for (var i = 0; i <= servings; i++) {
+		eggs = eggsNeeded * servings;
+		milk = milkNeeded * servings;
+		banana = bananaNeeded * servings;
+		flour = flourNeeded * servings;
+		cinnamon = cinnamonNeeded * servings;
+/* 		sug */
+	}
+	 
+}
+
+function stringFunction(servings, partSentence) {
+	var strToReturn;
+	strToReturn = servings +' '+ partSentence;
+	return strToReturn;
 }
 
 
 // My Outputs
 
+console.log('Banana Bread time!');
+cookingQuestion = window.prompt = ('Will you be cooking banana bread tonight?', 'yes/no');
 
+var timeToCook = procedureFunction();
+
+if (timeToCook) {
+	ingredientsConfirm = window.confirm('Do you have the ingredient needed');
+	
+	if (ingredientsConfirm) {
+		var checkIng = booleanFunction(ingredientsConfirm, arg);
+		if (!checkIng)
+			console.log('Go to the store!');
+		else {
+			var numServings = numberFunction(6); //6 servings
+			var doneCooking = stringFunction(numServings.toString(), 'I have made');
+			console.log(doneCooking);
+		}
+	} else {
+		console.log('Well, check the fridge, Jack!');
+	}
+	
+} else {
+	console.log('Guess it\'s not time, maybe tomorrow');
+}
 
 
 
